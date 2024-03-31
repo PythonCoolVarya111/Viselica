17d2801ca5106141d451ffb54cdaea11f06fbf4d
Zubareva Varya <varvara14.108@icloud.com>
2024-03-31 19:01:05 +0300

    Добавили функцию и нарисовали человека без головы

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from turtle import *
-screensize(600,600)
-# bgpic("elka.png")
-# hideturtle()
+
+
+def draw_line(point1: list, point2: list):
+    penup()
+    goto(point1[0], point1[1])
+    pendown()
+    goto(point2[0], point2[1])
+    penup()
+
+
+Screen().setup(1200, 1200)
 shape("turtle")
 penup()
 
-# украшаем елку
-goto(80,170)
-dot(40,"red")
-# ваш код тут...
-
-# _
+draw_line([-500, -250], [-100, -250])
+draw_line([-100, -250], [-100, 200])
+draw_line([-100, 200], [-300, 200])
+draw_line([-300, 200], [-300, 100])
+draw_line([-300, 40], [-300, -100])
+draw_line([-300, 40], [-240, 0])
+draw_line([-300, 40], [-360, 0])
+draw_line([-300, -100], [-230, -170])
+draw_line([-300, -100], [-370, -170])
 mainloop()
