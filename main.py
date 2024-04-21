fb7f3cfdd34e9a6f3704b41c303158ce4b3e14dd
Zubareva Varya <varvara14.108@icloud.com>
2024-04-21 19:01:00 +0300

    кнопка алфавит

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from turtle import *
 
+#global
+alphabet_dict = {}
+squareCoods = []
+
 #функции:
 
 def draw_line(point1, point2, size=3, p_color='black') -> object:
@@ -37,21 +41,24 @@ def draw_square(top_left, len_edge = 60, size=3, p_color='black'):
     draw_line(points[3], points[0], size, p_color)
 
 def draw_zagadka(word):
+    global squareCoods
     lenSquare = 60
     interval = 35
     screenWidth = Screen().window_width()
     wordWidth = (len(word) * lenSquare) + (len(word) - 1) * interval
+    squareCoods = []
     x = (screenWidth // 2 * -1) + (screenWidth - wordWidth) // 2
     startPoint = [x, -300]
     for x in range(len(word)):
         draw_square(startPoint, lenSquare + 20)
         startPoint[0] += lenSquare + interval
-
+        squareCoods.append(startPoint)
+    print(squareCoods)
 def draw_alphabet():
+    global alphabet_dict
     x = 100
     y = 250
     alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-    alphabet_dict = {}
     for i in range(len(alphabet)):
         if i % 5 == 0 and i != 0:
             x = 100
@@ -60,6 +67,19 @@ def draw_alphabet():
         write(alphabet[i], align='center', font=('Comic Sans MS', 40, 'bold' ))
         alphabet_dict[alphabet[i]] = (xcor(), ycor()+30)
         x += 100
+
+def button_alphabet(x, y):
+    global alphabet_dict
+    for key, centerCoord in alphabet_dict.items():
+        if abs(centerCoord[0] - x) < 100//2 and abs(centerCoord[1] - y) < 80 // 2:
+            print(key)
+
+
+def set_letter(letter, numsletter:list):
+    for num in numsletter:
+        letterPos = (squareCoods[num][0] + 100 // 2, squareCoods[num][1] - 80 // 2)
+        goto(letterPos)
+        write(alphabet[i], align='center', font=('Comic Sans MS', 40, 'bold'))
 
 
 #экран
@@ -70,7 +90,7 @@ Screen().setup(1200, 900)
 #отображение черепашки:
 
 shape("turtle")
-speed(3)
+speed(0)
 penup()
 
 
@@ -93,12 +113,12 @@ draw_line([-300, -100], [-370, -170])
 
 #рисуем квадраты для слова:
 
-draw_zagadka('fgfa')
+draw_zagadka('кот')
 
 
 #пишем алфавит:
 
 draw_alphabet()
 
-
+onscreenclick(button_alphabet)
 mainloop()
